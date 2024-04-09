8a9a2b85ebe4eefb0fe2349243327607c8d18a1b
stekap000 <stekap000@gmail.com>
2024-04-10 01:08:45 +0200

    Structure setup

--- code/compression.cpp
+++ code/compression.cpp
@@ -1,5 +1,33 @@
 #include "compression.hpp"
 
+/*
+  Normalized Information Distance:
+
+  max{K(x|y), K(y|x)}
+  This is distance measure between two objects in the context of Kolmogorov complexity.
+  Reason for taking "max" is the following:
+  K(x|y) - Minimal complexity of the program that will move us from y to x.
+  K(y|x) - Minimal complexity of the program that will move us from x to y.
+  Additionally, if some complexity "K" is needed for movement from "a" to "b", then any
+  complexity larger than "K" is also capable of moving us from "a" to "b".
+  With this in mind, by taking "max", we guarantee that we will end up with the complexity
+  capable of moving us in both directions ie. it is invariant under direction, which is needed
+  for standard notion of distance d(x,y)=d(y,x). In other words we are finding length of minimal
+  program capable of both translations.
+
+  
+  
+  NID(x,y) = max{K(x|y), K(y|x)} / max{K(x), K(y)}
+           = (K(y) - I(x:y)) / K(y), I(x:y) = K(y) - K(y|x)
+*/
+
+#include <vector>
+
+#include "util.hpp"
+
 namespace ctc {
-	
+	void lzw(u16 dict_size, std::string) {
+		std::vector<u16> *out = new std::vector<u16>();
+		
+	}
 }
--- code/compression.hpp
+++ code/compression.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <string>
+#include "util.hpp"
+
 namespace ctc {
-	
+	void lzw(u16 dict_size, std::string);
 }
