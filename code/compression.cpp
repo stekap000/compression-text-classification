15529a15ea265166b1270cdb637b5bc0a6c9ce63
stekap000 <stekap000@gmail.com>
2024-04-13 00:06:08 +0200

    lzw decode

--- code/compression.cpp
+++ code/compression.cpp
@@ -1,30 +1,8 @@
-#include "compression.hpp"
-
-/*
-  Normalized Information Distance:
-
-  max{K(x|y), K(y|x)}
-  This is distance measure between two objects in the context of Kolmogorov complexity.
-  Reason for taking "max" is the following:
-  K(x|y) - Minimal complexity of the program that will move us from y to x.
-  K(y|x) - Minimal complexity of the program that will move us from x to y.
-  Additionally, if some complexity "K" is needed for movement from "a" to "b", then any
-  complexity larger than "K" is also capable of moving us from "a" to "b".
-  With this in mind, by taking "max", we guarantee that we will end up with the complexity
-  capable of moving us in both directions ie. it is invariant under direction, which is needed
-  for standard notion of distance d(x,y)=d(y,x). In other words we are finding length of minimal
-  program capable of both translations.
-
-  
-  
-  NID(x,y) = max{K(x|y), K(y|x)} / max{K(x), K(y)}
-           = (K(y) - I(x:y)) / K(y), I(x:y) = K(y) - K(y|x)
-*/
-
 #include <vector>
 #include <unordered_map>
 #include <iostream>
 
+#include "compression.hpp"
 #include "util.hpp"
 
 namespace ctc {
@@ -46,8 +24,10 @@ namespace ctc {
 				out->push_back(dict->at(token));
 				
 				// If we did not exceed dictionary limit.
-				if(next_code < dict_size) 
+				if(next_code < dict_size) {
 					(*dict)[token_wide] = next_code++;
+					//std::cout << token_wide << " === " << (*dict)[token_wide] << std::endl;
+				}
 				
 				token_wide = *it;
 			}
@@ -61,5 +41,30 @@ namespace ctc {
 		return *out;
 	}
 
+	std::string &lzw_decode(u16 dict_size, std::vector<u16> &comp_text) {
+		std::string *out = new std::string();
+		std::unordered_map<u16, std::string> *dict = new std::unordered_map<u16, std::string>();
 
+		for(u16 i = 0; i < 256; ++i)
+			(*dict)[i] = std::string(1, (char)i);
+
+		u16 next_code = 256;
+		std::string prev_token;
+		std::string token;
+		for(std::vector<u16>::iterator it = comp_text.begin(); it != comp_text.end(); ++it) {
+			if(dict->find(*it) != dict->end())
+				token = (*dict)[*it];
+
+			if(next_code < dict_size && prev_token.size() != 0) {
+				(*dict)[next_code++] = prev_token + token[0];
+				//std::cout << next_code - 1 << " === " << (*dict)[next_code - 1] << std::endl;
+			}
+			
+			*out = *out + dict->at(*it);
+			//std::cout << dict->at(*it);
+			prev_token = token;
+		}
+
+		return *out;
+	}
 }
