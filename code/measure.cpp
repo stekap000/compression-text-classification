3f0ae2eb7bf388d40f16492ba38fefce92d5bb47
stekap000 <stekap000@gmail.com>
2024-04-13 00:06:43 +0200

    measure init

---
+++ code/measure.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include "measure.hpp"
+#include "compression.hpp"
+
+/*
+  Normalized Information Distance:
+
+  max{K(x|y), K(y|x)}
+  This is distance measure between two objects in the context of Kolmogorov complexity.
+  Reason for taking "max" is the following:
+  K(x|y) - Minimal complexity of the program that will move us from y to x.
+  K(y|x) - Minimal complexity of the program that will move us from x to y.
+  Additionally, if some complexity "K" is needed for movement from "a" to "b", then any
+  complexity larger than "K" is also capable of moving us from "a" to "b".
+  With this in mind, by taking "max", we guarantee that we will end up with the complexity
+  capable of moving us in both directions ie. it is invariant under direction, which is needed
+  for standard notion of distance d(x,y)=d(y,x). In other words we are finding length of minimal
+  program capable of both translations.
+
+  
+  
+  NID(x,y) = max{K(x|y), K(y|x)} / max{K(x), K(y)}
+           = (K(y) - I(x:y)) / K(y), I(x:y) = K(y) - K(y|x)
+*/
+
+namespace ctc {
+
+}
---
+++ code/measure.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace ctc {
+	int distance(std::string a, std::string b);
+}
